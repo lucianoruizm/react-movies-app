ec409eacacfe0f0d770827590858ab7658e3f6b6
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-17 18:59:01 -0300

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import { MoviesGrid } from './components/MoviesGrid';
 import styles from './App.module.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { MovieDetails } from './pages/MovieDetails';
+import { LandingPage } from './pages/LandingPage';
 
 export function App() {
   return (
-    <div>
+    <Router>
       <header>
-        <h1 className={styles.title}>Movies</h1>
+        <Link to="/">
+          <h1 className={styles.title}>Movies</h1>
+        </Link>
+        <Link to="/">Home</Link>
+        <Link to="/movie">Movies</Link>
       </header>
       <main>
-        <MoviesGrid />
+      <Switch>
+          <Route exact path="/movie">
+            <MovieDetails />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
       </main>
-    </div>
+    </Router>
   );
 }
