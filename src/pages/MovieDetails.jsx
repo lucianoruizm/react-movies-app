d194b5352e2d831779563c07f0c24546ca9c0041
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-19 12:46:55 -0300

    .

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,9 +5,13 @@ export function MovieDetails() {
     const imageUrl = "http://image.tmdb.org/t/p/w500" + movie.poster_path;
     return ( 
       <div className={styles.detailsContainer}>
-        <img src={imageUrl} alt={movie.title} />
-        <div>
-          <p><strong>Title</strong>: {movie.title}</p>
+        <img 
+          className= {`${styles.col} ${styles.movieImage}`}
+          src={imageUrl} 
+          alt={movie.title} 
+        />
+        <div className={`${styles.col} ${styles.movieDetails}`}>
+          <p className={styles.fistItem}><strong>Title</strong>: {movie.title}</p>
           <p><strong>Genres</strong>: {movie.genres.map(genre => genre.name).join(", ")}</p>
           <p><strong>Description</strong>: {movie.overview}</p>
         </div>
