71f3fc14a2b35dbbf38dc0d39150c7ac350c5e1e
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-20 23:21:11 -0300

    search

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,17 +1,37 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { get } from '../utils/httpClient';
 import { MoviesCard } from './MoviesCard';
 import styles from './MoviesGrid.module.css';
+import { Spinner } from './Spinner';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
 
 export function MoviesGrid () {
 
-    const [movies, setMovies] = useState([]);;
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     
+    const query = useQuery();
+    const search = query.get("search");
+    console.log(search);
+
     useEffect(() => {
-        get("/discover/movie").then(data => {
+        const searchUrl = search 
+        ? "/search/movie?query=" + search 
+        : "/discover/movie";
+        setIsLoading(true);
+        get(searchUrl).then(data => {
             setMovies(data.results);
+            setIsLoading(false);
           });
-    }, []);
+    }, [search]);
+
+    if (isLoading) {
+        return <Spinner />;
+    }
 
     return (
         <ul className={styles.moviesGrid}>
